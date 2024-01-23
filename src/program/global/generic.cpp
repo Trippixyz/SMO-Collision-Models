d3e4b02cd1eeba73b83a6f266b9139deccd28cef
Trippixyz <felix.edyou@gmail.com>
2024-01-23 10:47:23 +0100

    Removed unnecessary includes

--- src/program/global/generic.cpp
+++ src/program/global/generic.cpp
@@ -1,11 +1,5 @@
 #include "generic.h"
 
-#include "al/Library/Player/PlayerUtil.h"
-#include "al/Library/Scene/SceneUtil.h"
-
-#include "RedStar/Player/PlayerActorHakoniwa.h"
-#include "RedStar/Scene/StageScene.h"
-
 namespace global {
 
     GINT_Impl(stageSceneTimer)
